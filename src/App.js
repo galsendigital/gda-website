cd66dd6ace0b9e0ca151a7eca65f8c3102103f48
Moustapha Kachab <moustaphakachab@esp.sn>
2022-06-30 04:25:54 +0000

    Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.scss';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefit from './components/Benefit';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App w-full flex flex-col flex-wrap justify-center items-center">
@@ -10,6 +12,7 @@ function App() {
         <Hero />
         <Benefit />
       </main>
+      <Footer />
     </div>
   );
 }
